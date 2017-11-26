b7d6e2f0206f4bad4671fbef8a2be1c153c7ca49
woshimanong1990 <qinghelanzhu1@126.com>
2017-11-26 22:09:13 +0800

    code change

--- user_info/models.py
+++ user_info/models.py
@@ -2,58 +2,58 @@
 from django.db import models
 from django.contrib.auth.models import User
 from common_utils.models import CommonModel
-from .variables import GENDER, INCOME, MARRY_STATE, INDUSTRY, PROFESSION, THREE_STATE, BODY_SHAPE, STAR_SIGN, NATION
+from .variables import GENDER, INCOME, MARRY_STATE, INDUSTRY, PROFESSION, THREE_STATE, BODY_SHAPE, STAR_SIGN, NATION, EDUCATION
 
 
 class UserInfo(CommonModel):
     user = models.OneToOneField(User)
-    gender = models.CharField(GENDER, default="M", verbose_name="性别")
+    gender = models.CharField(choices=GENDER, default="M", verbose_name="性别")
     age = models.IntegerField(verbose_name="年龄")
     height = models.SmallIntegerField(verbose_name="身高")
     birthday = models.DateField(verbose_name="出生日期")
     enable = models.BooleanField(default=True, verbose_name="是否可用")
     live_location = models.CharField(verbose_name="现居住地")
-    birth_location = models.CharField(verbose_name="籍贯")
-    educational_background = models.IntegerField(verbose_name="学历")
-    industry = models.CharField(verbose_name="从事行业", choices=INDUSTRY, default="100")
-    occupation = models.CharField(verbose_name="职业", choices=PROFESSION, default="10000")
-    year_income = models.CharField(verbose_name="年收入", choices=INCOME)
+    birth_location = models.CharField(verbose_name="籍贯", default="")
+    educational_background = models.CharField(verbose_name="学历", choices=EDUCATION, default="-1")
+    industry = models.CharField(verbose_name="从事行业", choices=INDUSTRY, default="-1")
+    occupation = models.CharField(verbose_name="职业", choices=PROFESSION, default="-1")
+    year_income = models.CharField(verbose_name="年收入", choices=INCOME, default="-1")
     email = models.EmailField(blank=True, null=True, verbose_name="邮箱")
-    marry_state = models.CharField(verbose_name="婚姻状况", choices=MARRY_STATE)
-    has_children = models.CharField(verbose_name="是否有小孩", choices=THREE_STATE, default='0')
-    has_house = models.CharField(verbose_name="是否有房", choices=THREE_STATE, default='0')
-    has_car = models.CharField(verbose_name="是否有车", choices=THREE_STATE, default='0')
+    marry_state = models.CharField(verbose_name="婚姻状况", choices=MARRY_STATE, default="-1")
+    has_children = models.CharField(verbose_name="是否有小孩", choices=THREE_STATE, default='-1')
+    has_house = models.CharField(verbose_name="是否有房", choices=THREE_STATE, default='-1')
+    has_car = models.CharField(verbose_name="是否有车", choices=THREE_STATE, default='-1')
     weight = models.IntegerField(verbose_name='体重', default=0)
-    shape = models.CharField(verbose_name='体型', choices=BODY_SHAPE, default='100')
-    is_smoking = models.CharField(verbose_name='是否吸烟', choices=THREE_STATE, default='0')
-    star_sigh = models.CharField(verbose_name='星座', choices=STAR_SIGN, default="100")
-    nation = models.CharField(verbose_name='民族', choices=NATION, default="100")
+    shape = models.CharField(verbose_name='体型', choices=BODY_SHAPE, default='-1')
+    is_smoking = models.CharField(verbose_name='是否吸烟', choices=THREE_STATE, default='-1')
+    star_sigh = models.CharField(verbose_name='星座', choices=STAR_SIGN, default="-1")
+    nation = models.CharField(verbose_name='民族', choices=NATION, default="-1")
 
     class Meta:
         verbose_name = "用户信息"
         verbose_name_plural = "用户信息"
 
 
-class DesireCondition(models):
+class DesireCondition(CommonModel):
     user = models.OneToOneField(User)
     age = models.CharField(verbose_name="年龄", default='不限')
     height = models.CharField(verbose_name="身高", default='不限')
     enable = models.BooleanField(default=True, verbose_name="是否可用")
     live_location = models.CharField(verbose_name="现居住地", default='不限')
-    birth_location = models.CharField(verbose_name="籍贯")
-    educational_background = models.IntegerField(verbose_name="学历")
-    industry = models.CharField(verbose_name="从事行业", choices=INDUSTRY, default="100")
-    occupation = models.CharField(verbose_name="职业", choices=PROFESSION, default="10000")
-    year_income = models.CharField(verbose_name="年收入", default='不限')
-    marry_state = models.CharField(verbose_name="婚姻状况", choices=MARRY_STATE)
-    has_children = models.CharField(verbose_name="是否有小孩", choices=THREE_STATE, default='0')
-    has_house = models.CharField(verbose_name="是否有房", choices=THREE_STATE, default='0')
-    has_car = models.CharField(verbose_name="是否有车", choices=THREE_STATE, default='0')
+    birth_location = models.CharField(verbose_name="籍贯", default='不限')
+    educational_background = models.CharField(verbose_name="学历", )
+    industry = models.CharField(verbose_name="从事行业", choices=INDUSTRY, default="-1")
+    occupation = models.CharField(verbose_name="职业", choices=PROFESSION, default="-1")
+    year_income = models.CharField(verbose_name="年收入", choices=PROFESSION, default="-1")
+    marry_state = models.CharField(verbose_name="婚姻状况", choices=MARRY_STATE, default="-1")
+    has_children = models.CharField(verbose_name="是否有小孩", choices=THREE_STATE, default='-1')
+    has_house = models.CharField(verbose_name="是否有房", choices=THREE_STATE, default='-1')
+    has_car = models.CharField(verbose_name="是否有车", choices=THREE_STATE, default='-1')
     weight = models.IntegerField(verbose_name='体重', default=0)
-    shape = models.CharField(verbose_name='体型', choices=BODY_SHAPE, default='100')
-    is_smoking = models.CharField(verbose_name='是否吸烟', choices=THREE_STATE, default='0')
-    star_sigh = models.CharField(verbose_name='星座', choices=STAR_SIGN, default="100")
-    nation = models.CharField(verbose_name='民族', choices=NATION, default="100")
+    shape = models.CharField(verbose_name='体型', choices=BODY_SHAPE, default='-1')
+    is_smoking = models.CharField(verbose_name='是否吸烟', choices=THREE_STATE, default='-1')
+    star_sigh = models.CharField(verbose_name='星座', choices=STAR_SIGN, default="-1")
+    nation = models.CharField(verbose_name='民族', choices=NATION, default="-1")
 
     class Meta:
         verbose_name = "婚恋理想条件"
